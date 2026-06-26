064689690978d64d111617adf9966e84b67722a6
ioshY-k <joshyk1307@gmail.com>
2026-06-26 23:16:57 +0200

    Include tutorials

--- Objects/collectable_trick.gd
+++ Objects/collectable_trick.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 @onready var trick_animation: AnimationPlayer = $TrickAnimation
+@onready var collect_particle: CPUParticles2D = $CollectParticle
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	collect_particle.emitting = true
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SPRING)
+	tween.tween_property(self, "scale", scale * 2, 0.2)
+	tween.tween_property(self, "scale", scale, 0.14)
 	trick_animation.play("Trick")
 	
